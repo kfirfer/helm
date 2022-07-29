ade3087338e9a7ba1e467705509eecc56483e612
Kfir Fersht <kfir.fersht@plainid.com>
2022-07-29 15:14:24 +0300

    [mysqldump] fix template

--- makefiles/helm-charts.mk
+++ makefiles/helm-charts.mk
@@ -3,3 +3,8 @@
 
 helm.charts.install: ## var: chart,releasename
 	helm upgrade -i ${releasename} charts/${chart} --namespace=default
+
+
+helm.template.test:
+	helm template mysqldump charts/mysqldump --values charts/mysqldump/values-test.yaml --debug
+
